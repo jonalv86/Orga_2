f325db683c94a8191f91b6aef4ddc248c89d5dc6
Jonatan Alvarez <jony.alv.dev@outlook.com>
2018-04-24 22:32:21 -0300

    problemas con el borrar.

--- Teoria/Listas.c
+++ Teoria/Listas.c
@@ -1,25 +1,87 @@
-struct nodo_lista {
+#include <stdio.h>
+#include <stdlib.h>
+
+struct nodo_abb {
 	int valor;
-	struct nodo_lista * prox;
+	int cantidad;
+	struct nodo_abb *izq;
+	struct nodo_abb *der;
 };
 
-struct nodoLista * agregar_valor (struct nodo_lista * anterior, int a) {
-	if (anterior == NULL) {
-		struct nodo_lista * aux = malloc(sizeof(struct nodo_lista));
-		aux -> valor = a;
-		aux -> prox = NULL;
-		return aux;
+struct nodo_abb * agregar_abb(struct nodo_abb * arbol, int val) {
+	struct nodo_abb *nuevo = (struct nodo_abb *) malloc(sizeof(struct nodo_abb));
+	if (arbol == NULL) {
+		nuevo -> valor = val;
+		nuevo -> cantidad = 1;
+		nuevo -> izq = NULL;
+		nuevo->der = NULL;
+		return nuevo;
 	}
-	return agregar_valor (anterior -> prox, a);
+	int valorActual = arbol -> valor;
+	if (valorActual == val) {
+		arbol -> cantidad++;
+	} else if (val < valorActual) {
+		arbol->izq = agregar_abb(arbol->izq, val);
+	} else {
+		arbol->der = agregar_abb(arbol->der, val);
+	}
+	return arbol;
 }
 
-void eliminar_lista (struct nodo_lista * nodo) {
-	if (nodo == NULL) {
-		return
+void borrar_abb(struct nodo_abb * a) {
+	if (a == NULL) {
+		return;
 	}
-	if (nodo -> prox != NULL) {
-		eliminar_lista (nodo -> prox);
+	if (a->izq != NULL) {
+		borrar_abb (a->izq);
+		a->izq = NULL;
 	}
-	free (nodo);
+	if (a->der != NULL) {
+		borrar_abb (a->der);
+		a->der = NULL;
+	}
+	a = NULL;
+	free (a);
 	return;
+}
+
+void mostrar_abb(struct nodo_abb * a) {
+	printf("{");
+	if (a != NULL) {
+		printf("%d:%d ", a -> valor, a -> cantidad);
+		mostrar_abb(a -> izq);
+		printf(" ");
+		mostrar_abb(a -> der);
+	}
+	printf("}");
+}
+
+main(int argc, char *argv[]) {
+	struct nodo_abb *inicial;
+	inicial = agregar_abb(NULL, 8);
+	//mostrar_abb(inicial);
+	//printf("\n");	
+	agregar_abb(inicial, 54);
+	//mostrar_abb(inicial);
+	//printf("\n");	
+	agregar_abb(inicial, 17);
+	//mostrar_abb(inicial);
+	//printf("\n");	
+	agregar_abb(inicial, 5);
+	//mostrar_abb(inicial);
+	//printf("\n");	
+	//agregar_abb(inicial, 8);
+	//mostrar_abb(inicial);
+	//printf("\n");	
+	//agregar_abb(inicial, 5);
+	//mostrar_abb(inicial);
+	//printf("\n");	
+	agregar_abb(inicial, 70);
+	//mostrar_abb(inicial);
+	//printf("\n");	
+	agregar_abb(inicial, 2);
+	mostrar_abb(inicial);
+	printf("\n");
+	borrar_abb(inicial);
+	mostrar_abb(inicial);
 }
