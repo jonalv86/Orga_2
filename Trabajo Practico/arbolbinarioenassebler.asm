4741c81cc2b67b03802fbc64499481d50aba9b3a
Unknown <jony.alv.dev@outlook.com>
2018-04-27 21:29:04 -0300

    Funciones terminadas en assembler

--- Trabajo Practico/arbolbinarioenassebler.asm
+++ Trabajo Practico/arbolbinarioenassebler.asm
@@ -2,7 +2,7 @@
 
 section .data   ;para testear
    ; inicial:    DD 0x0
-    inicial:    times 4 DD 0
+    inicial:    times 400000 DD 0
 
 section .text
 global CMAIN
@@ -17,12 +17,120 @@ CMAIN:
     CALL    agregar_abb
     ADD     ESP, 8
     ;Fin add
+    
     ;Inicio print
     MOV     EAX, inicial
     PUSH    EAX
     CALL    mostrar_abb
     ADD     ESP, 4
     ;Fin print
+    
+    NEWLINE
+    
+    ;Inicio add
+    MOV     EAX, inicial
+    MOV     EBX, 54
+    PUSH    EBX
+    PUSH    EAX
+    CALL    agregar_abb
+    ADD     ESP, 8
+    ;Fin add
+    
+    ;Inicio add
+    MOV     EAX, inicial
+    MOV     EBX, 17
+    PUSH    EBX
+    PUSH    EAX
+    CALL    agregar_abb
+    ADD     ESP, 8
+    ;Fin add
+    
+    ;Inicio add
+    MOV     EAX, inicial
+    MOV     EBX, 5
+    PUSH    EBX
+    PUSH    EAX
+    CALL    agregar_abb
+    ADD     ESP, 8
+    ;Fin add
+    
+    ;Inicio add
+    MOV     EAX, inicial
+    MOV     EBX, 8
+    PUSH    EBX
+    PUSH    EAX
+    CALL    agregar_abb
+    ADD     ESP, 8
+    ;Fin add
+    
+    ;Inicio add
+    MOV     EAX, inicial
+    MOV     EBX, 5
+    PUSH    EBX
+    PUSH    EAX
+    CALL    agregar_abb
+    ADD     ESP, 8
+    ;Fin add
+    
+    ;Inicio add
+    MOV     EAX, inicial
+    MOV     EBX, 70
+    PUSH    EBX
+    PUSH    EAX
+    CALL    agregar_abb
+    ADD     ESP, 8
+    ;Fin add
+    
+    ;Inicio add
+    MOV     EAX, inicial
+    MOV     EBX, 75
+    PUSH    EBX
+    PUSH    EAX
+    CALL    agregar_abb
+    ADD     ESP, 8
+    ;Fin add
+    
+    ;Inicio add
+    MOV     EAX, inicial
+    MOV     EBX, 2
+    PUSH    EBX
+    PUSH    EAX
+    CALL    agregar_abb
+    ADD     ESP, 8
+    ;Fin add
+    
+    ;Inicio add
+    MOV     EAX, inicial
+    MOV     EBX, 4
+    PUSH    EBX
+    PUSH    EAX
+    CALL    agregar_abb
+    ADD     ESP, 8
+    ;Fin add
+    
+    ;Inicio print
+    MOV     EAX, inicial
+    PUSH    EAX
+    CALL    mostrar_abb
+    ADD     ESP, 4
+    ;Fin print
+    
+    NEWLINE
+    
+    ;Inicio borrar
+    MOV     EAX, inicial
+    PUSH    EAX
+    CALL    borrar_abb
+    ADD     ESP, 4
+    ;Fin borrar
+    
+    ;Inicio print
+    MOV     EAX, inicial
+    PUSH    EAX
+    CALL    mostrar_abb
+    ADD     ESP, 4
+    ;Fin print
+    
     xor eax, eax
     ret
 
@@ -49,12 +157,38 @@ agregar_abb:
 existe_arbol:
     ;No implementado
     ;Mismo valor
-    MOV
-    CMP     EBX
-    ;Mayor valor
+    MOV     ECX, [EBP+12]
+    MOV     EDX, [EAX]
+    CMP     ECX, EDX
+    JE      incrementar_nodo
     ;Menor valor
+    JL      nodo_menor
+    ;Mayor valor
+    JA      nodo_mayor
+    
+incrementar_nodo:
+    ADD     EAX, 4
+    ADD     [EAX], dword 1
     JMP     SALIR
     
+nodo_menor:
+    ;Inicio add
+    ADD     EAX, 8
+    JMP     nuevo_nodo
+
+nodo_mayor:
+    ADD     EAX, 12
+    
+nuevo_nodo:    
+    ;Inicio add
+    ;MOV     EBX, [EAX]
+    PUSH    ECX ;valor
+    PUSH    EAX
+    CALL    agregar_abb
+    ADD     ESP, 8
+    ;Fin add
+    JMP     SALIR
+        
 mostrar_abb:
     ;implentado recursivo
     PUSH    EBP
@@ -73,18 +207,31 @@ mostrar_abb:
     PRINT_STRING " "
     ;mostrar nodo izquierdo
     ADD     EAX, 4
-    MOV     EBX, [EAX];Testear
-    PUSH    EBX
+    ;MOV     EBX, [EAX];Testear
+    PUSH    EAX
     CALL    mostrar_abb
     ADD     ESP, 4
     PRINT_STRING " "
     ;mostrar nodo derecho
     ADD     EAX, 4
-    MOV     EBX, [EAX];Testear
-    PUSH    EBX
+    ;MOV     EBX, [EAX];Testear
+    PUSH    EAX
     CALL    mostrar_abb
     ADD     ESP, 4    
     JMP     TERMINARMOSTRAR
+    
+borrar_abb:
+    PUSH    EBP
+    MOV	    EBP, ESP
+    MOV     EAX, [EBP+8]
+    MOV     EBX, [EAX]
+    CMP     EBX, 0
+    JE      SALIR
+    ADD     EAX, 8
+    MOV     [EAX], dword 0
+    ADD     EAX, 4
+    MOV     [EAX], dword 0
+    JMP     SALIR
          
 SALIR:
     POP     EBP
