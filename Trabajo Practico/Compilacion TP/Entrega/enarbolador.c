bfc9d9a4195a9e74fb300b8d60621ce26deb6372
Unknown <jony.alv.dev@outlook.com>
2018-05-09 16:09:34 -0300

    Un poco más prolijo.

--- Trabajo Practico/Compilacion TP/Entrega/enarbolador.c
+++ Trabajo Practico/Compilacion TP/Entrega/enarbolador.c
@@ -21,17 +21,17 @@ void enOrden(struct nodo_abb * a)
 		enOrden(a->der);
 	}
 }
-
+//á: 160 é: 130 í: 161 ó: 162 ú: 163 Á: 181 É: 144 Í: 214 Ó: 224 Ú: 23 ñ: 164 Ñ: 165
 int main(int argc, char *argv[]) {
 	int inicioArbol = 0;
 	int valor;
 	struct nodo_abb *inicial;
 
+	printf("Bienvenido al ordenador de n%cmeros.\nIngrese a continuaci%cn los n%cmeros que desea ordenar uno por l%cnea y luego cualquier otro caracter para finalizar:\n\n",163,162,163,161);
 	int esNumero = scanf("%d", &valor);
 	while(getchar() != '\n');		//Consumir el buffer.
 		
-	while (esNumero == 1)
-	{
+	while (esNumero == 1) {
 		if (inicioArbol == 0) {
 			inicial = agregar_abb(NULL, valor);
 			inicioArbol = 1;
@@ -42,8 +42,9 @@ int main(int argc, char *argv[]) {
 		while(getchar() != '\n'); 
 	}
 	if (inicioArbol == 0) {
-		printf("No se han ingresado valores y por lo tanto no se ha harmado el arbol.\n");
+		printf("No se han ingresado valores y por lo tanto no se ha harmado el %crbol.\n",160);
 	} else {
+		printf("\n\nN%cmeros ordenados:\n",163);
 		enOrden(inicial);
 	}
 	printf("\nPresione enter para salir.\n");
