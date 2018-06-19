a0eddbd5fea24b5aacaf7147b54e977b489d1f06
Unknown <jony.alv.dev@outlook.com>
2018-06-18 22:22:51 -0300

    Interpolar array de bytes

--- Trabajo practico 2/funcion_interpolar.asm
+++ Trabajo practico 2/funcion_interpolar.asm
@@ -2,7 +2,7 @@
 section .data
 
 ;testing general
-;vector: dd 30, 60, 90, 120
+;vector: dd 30.0, 60.0, 90.0, 120.0
 ;vector2: dd 0.5, 0.5, 0.5, 0.5
 ;radio:  dd 1.7
 ;constante: dd 1,1,1,1
@@ -10,9 +10,9 @@ section .data
 ;variable: dd 0.4
 
 ;testing funcion
-;img1:       dd  32, 16, 8, 4, 32;, 16, 8
-;img2:       dd  4, 4, 4, 4, 4;, 4, 4
-;resultado:  dd  0,0,0,0,0;,0,0
+;img1:       db  8, 16, 24, 32, 64;, 16, 8
+;img2:       db  4, 4, 4, 4, 4;, 4, 4
+;resultado:  db  0,0,0,0,0;,0,0
 ;p:          dd  0.5
 ;cantidad:   dd  5
 
@@ -21,6 +21,8 @@ section .data
 constanteuno: dd 1.0
 multvuno: dd 0, 0, 0, 0
 multvdos: dd 0, 0, 0, 0
+shuftoregistry: db 0,255,255,255,1,255,255,255,2,255,255,255,3,255,255,255
+shuftomemory:   db 0,4,8,12,255,255,255,255,255,255,255,255,255,255,255,255
 
 ;void interpolar(unsigned char *img1, 
 ;                unsigned char *img2,
@@ -33,7 +35,7 @@ global _interpolar
 section .text
 ;global CMAIN
 ;CMAIN:
-;    mov ebp, esp; for correct debugging
+    ;mov ebp, esp; for correct debugging
     ;write your code here
     ;r = p × v1 + (1 - p) × v2
     
@@ -43,7 +45,7 @@ section .text
     ;movups xmm0, [eax]
     ;movups xmm1, [ebx]
     ;mulps xmm0, xmm1
-    ;cvttps2dq xmm1, xmm1
+    ;cvttps2dq xmm0, xmm0
     
     ;testing 2
     ;fld dword [radio]
@@ -98,7 +100,7 @@ section .text
     ;movups xmm0, [eax]
     ;movq mm0, [eax]
     ;testing 6
-    ;add eax, 16
+    ;add eax, 4
     ;movups xmm1, [eax]
     ;mov eax, [cantidad]
     ;mov ebx, [p]
@@ -119,7 +121,6 @@ _interpolar:
     MOV         EAX,    [EBP+8]    ;Desde C siempre tiene que venir múltiplo de 4
     MOV         EBX,    [EBP+12]
     MOV         EDX,    [EBP+16]
-    ;MOV         ECX,    [EBP+20]
     ;Hacer un vector con la constante y alojar en XMM3
     FLD         dword   [EBP+20]
     MOV         ECX,    multvuno
@@ -148,7 +149,9 @@ _interpolar:
     MOVUPS      XMM3,   [ECX]
     
     XOR         ECX,    ECX
-    MOV         ECX,     [EBP+24]
+    MOV         ECX,    [EBP+24]
+    MOVUPS      XMM4,   [shuftoregistry]
+    MOVUPS      XMM5,   [shuftomemory]
 CICLOINTERPOLAR:
     CMP         ECX,     0
     JE          FINALIZARCICLO
@@ -157,22 +160,30 @@ CICLOINTERPOLAR:
     JBE         ACOMODAR
     ;r = v1 x p + (1 - p) × v2
     ;r = xmm0 x xmm2 + xmm3 x xmm1
+    ;Muevo
     MOVUPS      XMM0,   [EAX]
     MOVUPS      XMM1,   [EBX]
+    ;Acomodo a 32 bits
+    PSHUFB      XMM1,   XMM4
+    PSHUFB      XMM0,   XMM4
+    ;Convierto a float
+    CVTDQ2PS    XMM0,   XMM0
+    CVTDQ2PS    XMM1,   XMM1
+    ;Opero
     MULPS       XMM0,   XMM2
     MULPS       XMM1,   XMM3
     ADDPS       XMM0,   XMM1
     ;pasar los floats a enteros para guardar en [EDX]
-    ;cvtps2dq    XMM0,   XMM0
-    ;cvttps2dq   xmm0, xmm0
-    cvtps2dq    XMM4,   XMM0
-    cvttps2dq   xmm5, xmm0
-    ;Puede tirar segmentation fail
-    movups      [EDX],  XMM0
+    CVTPS2DQ    XMM0,   XMM0
+    ;Acomodo a 8 bits
+    PSHUFB      XMM0,   XMM5
+    ;Muevo a memoria
+    MOVDQ2Q     MM0,    XMM0
+    MOVD        [EDX],  MM0
     SUB         ECX,    4
-    ADD         EAX,    16
-    ADD         EBX,    16
-    ADD         EDX,    16
+    ADD         EAX,    4
+    ADD         EBX,    4
+    ADD         EDX,    4
     JMP         CICLOINTERPOLAR
 
 ACOMODAR:
@@ -181,9 +192,6 @@ ACOMODAR:
     MOV         EAX,    4
     SUB         EAX,    ECX
     MOV         ECX,    EAX
-    ADD         ECX,    EAX
-    ADD         ECX,    EAX
-    ADD         ECX,    EAX
     POP         EAX
     SUB         EAX,    ECX
     SUB         EBX,    ECX
